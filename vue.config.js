8a82c471bd65deba5e12b733704ae3713f2de538
chadm8 <martincc@dukes.jmu.edu>
2021-07-14 20:27:24 -0400

    deployment files

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,6 @@ module.exports = {
     proxy: 'https://query1.finance.yahoo.com/'
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/stock-market-stats/'
+    ? '/stock-market-stats-website/'
     : '/'
 };
