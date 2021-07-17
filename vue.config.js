45c4cbe6b4f3d39f4a640d088fdb6e81e701db14
chadm8 <martincc@dukes.jmu.edu>
2021-07-16 21:22:14 -0400

    migrate from GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,5 @@ module.exports = {
   ],
   devServer: {
     proxy: 'https://query1.finance.yahoo.com/'
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/stock-market-stats-website/'
-    : '/'
+  }
 };
